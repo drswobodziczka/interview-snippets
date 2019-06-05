60f647db5a70b71d4d585560ebf602144a66101a
Wawrzkowicz, Rafal <ext-rafal.wawrzkowicz@f-secure.com>
2019-06-05 19:01:06 +0200

    refactor: renamings

--- src/main/java/com/drswobod/excercices/java/AutoboxingUnboxing.java
+++ src/main/java/com/drswobod/excercices/java/AutoboxingUnboxing.java
@@ -1,19 +1,18 @@
 package com.drswobod.excercices.java;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
-
 class AutoboxingUnboxing {
 
-    // What should be the scope of Jupiter test method?
-    @Test
     void testBoxingAndUnboxing() {
-        Integer boxed = 127;
-        int unboxed = 127;
+        Integer boxedInt = 127;
+        int primitiveInt = 127;
 
-        System.out.println((boxed.equals(unboxed))); // ??
-        System.out.println((boxed == unboxed)); // ??
-        System.out.println((boxed == ((Integer) unboxed))); // ??
-        System.out.println((new Integer(boxed) == new Integer(unboxed))); // ??
+        System.out.println((boxedInt.equals(primitiveInt))); // ??
+        System.out.println((boxedInt == primitiveInt)); // ??
+        System.out.println((boxedInt == ((Integer) primitiveInt))); // ??
+        System.out.println((new Integer(boxedInt) == new Integer(primitiveInt))); // ??
+    }
+
+    public static void main(String[] args) {
+        new AutoboxingUnboxing().testBoxingAndUnboxing();
     }
 }
--- src/main/java/com/drswobod/excercices/java/SampleClasWithHashCode.java
+++ src/main/java/com/drswobod/excercices/java/SampleClasWithHashCode.java
@@ -3,9 +3,8 @@ package com.drswobod.excercices.java;
 import java.util.HashMap;
 import java.util.stream.IntStream;
 
-public class WhatsWrongWithThatSnippet {
+public class SampleClasWithHashCode {
 
-    // TODO: provide time measurement to show how bad is performance
     @Override
     public int hashCode() {
         return 1;
@@ -17,9 +16,9 @@ public class WhatsWrongWithThatSnippet {
     }
 
     public static void main(String[] args) {
-        HashMap<WhatsWrongWithThatSnippet, Integer> map = new HashMap<>();
+        HashMap<SampleClasWithHashCode, Integer> map = new HashMap<>();
         IntStream
                 .range(0,100)
-                .forEach(index -> map.put(new WhatsWrongWithThatSnippet(), index));
+                .forEach(index -> map.put(new SampleClasWithHashCode(), index));
     }
 }
